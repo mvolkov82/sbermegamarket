48287a79215dfdffccfde3105eb8f85a8861679f
MAlVolkov <leopard@inbox.ru>
2022-11-04 23:09:07 +0300

    Initial commit

---
+++ src/test/java/com/okeandra/demo/ApplicationStarterTests.java
@@ -0,0 +1,13 @@
+package com.okeandra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
